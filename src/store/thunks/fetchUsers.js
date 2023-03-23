2a91fe3836cd21c04a2e4c103ab150c678117633
Jesse Fang <n3phthys@MacBook-Pro.local>
2023-03-23 22:42:41 +0800

    add pause function to test loading

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,8 +4,17 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
 
+  //DEV ONLY!!
+  await pause(1000);
   //response.data === [{id:1,name:'Myra'}]
   return response.data;
 });
+
+//DEV ONLY!
+const pause = (duaration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duaration);
+  });
+};
 
 export { fetchUsers };
