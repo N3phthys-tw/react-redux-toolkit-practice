6ccfe3aad46d9415c2f44df81e6cf4fa6e4accbc
Jesse Fang <n3phthys@MacBook-Pro.local>
2023-03-24 16:56:42 +0800

    add skeleton comp and finish render users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,8 @@
+import { render } from "@testing-library/react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
 function UsersList() {
   const dispatch = useDispatch();
@@ -13,14 +15,24 @@ function UsersList() {
   }, [dispatch]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Skeleton times={6} className="h-10 w-full" />;
   }
 
   if (error) {
     return <div>Error fetching data...</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between item-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{renderedUsers}</div>;
 }
 
 export default UsersList;
