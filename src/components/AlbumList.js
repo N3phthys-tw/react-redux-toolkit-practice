8bcc866a86754fde74a9460c14a5aaca7d847af3
Jesse Fang <n3phthys@MacBook-Pro.local>
2023-03-27 20:59:15 +0800

    finish render albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -6,7 +6,28 @@ import Button from "./Button";
 function AlbumList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-  return <div>Albums for {user.name}</div>;
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumList;
