3279da3f92b32eca160b287d8614741eb2db5e17
Jesse Fang <n3phthys@MacBook-Pro.local>
2023-03-27 20:58:13 +0800

    finish create Api using RTK Query

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,4 +1,11 @@
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+
 function AlbumList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
   return <div>Albums for {user.name}</div>;
 }
 
