cb681d315680abe01ea3a439f7b7acb2e429b6fb
Jesse Fang <n3phthys@MacBook-Pro.local>
2023-03-28 20:56:11 +0800

    update tags design

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -47,9 +47,9 @@ const albumsApi = createApi({
       fetchAlbums: builder.query({
         providesTags: (result, eroor, user) => {
           const tags = result.map((album) => {
-            return { type: "Ablum", id: album.id };
+            return { type: "Album", id: album.id };
           });
-          tags.push({ type: "UsersAlbum", id: user.id });
+          tags.push({ type: "UsersAlbums", id: user.id });
           return tags;
         },
         query: (user) => {
