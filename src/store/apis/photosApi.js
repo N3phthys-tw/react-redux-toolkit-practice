6cd5ddb6aac935288b5e745069bab5973cbb5f3b
Jesse Fang <n3phthys@MacBook-Pro.local>
2023-03-28 21:10:43 +0800

    photos feature init

---
+++ src/store/apis/photosApi.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
+
+const photoApi = createApi({
+  reducerPath: "photos",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      fetchPhotos: builder.query({}),
+      addPhoto: builder.mutation({}),
+      removePhoto: builder.mutation({}),
+    };
+  },
+});
